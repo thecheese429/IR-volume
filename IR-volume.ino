1881a83f2169401b83015b2a71373308c48ba4ad
thecheese429 <thecheese429@hotmail.com>
2017-03-09 18:01:50 -0600

    Update code to accept input
    The code has been updated to complete the following tasks:
    
    Read the specific Samsung commands
    Identify when a button is held
    Increment the volume in larger steps when button is held
    Keep the volume within the legal range of (-79) - 0 dB

--- IR-volume.ino
+++ IR-volume.ino
@@ -6,20 +6,20 @@
 #include <IRLib.h>
 template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; } 
 
-#define DOWN 	0xfd48b7
-#define UP	 	0xfd48b6
-#define MUTE	0xfd48b5
-#define POWER	0xfd48b4
+#define DOWN 	0xE0E0D02F
+#define UP	 	0xE0E0E01F
+#define MUTE	0xE0E0F00F
+#define POWER	0xE0E040BF
 
 
 #define DEFAULTVOLUME -28	// the default volume used when power key is detected, or when mute is exited using a volume key
-#define	HOLDDELAY 200		//the amount of time delayed between volume increments when volume up/down is held
+#define	HOLDDELAY 400		//the amount of time delayed between volume increments when volume up/down is held
 
 int volume = DEFAULTVOLUME;
 int volumeAtMute = DEFAULTVOLUME;
-long Previous;
+long Previous[2] = {0,0};
 long timeHoldStarted = 0;
-long upOrDownPressTime = 0;
+long buttonPressTime[2] = {0,0};
 long timeHeld = 0;
 boolean mute = 0;
 boolean keyHeld = 0;
@@ -32,7 +32,7 @@ void setup()
 	IR.enableIRIn();
 	sendVolume();
 	
-	while(true)
+	while(false)
 	{
 		if(IR.GetResults(&decoder))
 		{
@@ -49,114 +49,142 @@ int increment()
 	if(keyHeld == 1)
 	{
 		timeHeld = millis() - timeHoldStarted; 
+		// Serial << "button held for " << timeHeld << "mS\n";
 	}
 	else
 	{
 		timeHeld = 0;
 	}
 	
-	if(timeHeld > 4000)
+	if(timeHeld > 1800)
 	{
-		return 6;
+		return 3;
 	}
-	else if(timeHeld > 1400)
+	else if(timeHeld > 1000)
 	{
-		return 4;
+		return 2;
 	}
 	else
 	{
-		return 2;
+		return 1;
 	}
 }
 
 void sendVolume()
 {
 	
-	int tens = -10 * (int)(volume/10);
-	int ones = -1 * (volume - tens * 10) ;
+	int tens = volume / -10;
+	int ones = -1 * (volume + tens * 10) ;
 	
-	Serial << "Setting volume to " << volume << "dB. Tens: " << tens << ", Ones: " << ones << "\n";
-	Serial << "tens command: ";
-	Serial.println(tens + B1110000, BIN);
-	Serial << "ones command: ";
-	Serial.println(ones + B1101000, BIN);
+	//Serial << "Setting volume to " << volume << "dB. Tens: " << tens << ", Ones: " << ones << "\n";
+	// Serial << "\ntens command: ";
+	// Serial.println(tens + B11100000, BIN);
+	// Serial << "ones command: ";
+	// Serial.println(ones + B11010000, BIN);
+	// Serial.println();
 	
 	/*
 	Wire.beginTransmission(10001000);
-	Wire.write( tens + B1110000);
-	Wire.write( ones + B1101000);
+	Wire.write( tens + B11100000);
+	Wire.write( ones + B11010000);
 	Wire.endTransmission();
 	*/
 }
 
 void loop()
 {
-	while(mute == 0)
+	
+	if(IR.GetResults(&decoder))
 	{
-		if(IR.GetResults(&decoder))
+		buttonPressTime[1] = buttonPressTime[0];
+		buttonPressTime[0] = millis();
+		
+		decoder.decode();
+		// Serial.println(decoder.value, HEX);
+		
+		
+		if(( Previous[0] == decoder.value || Previous[1] == decoder.value ) && ( buttonPressTime[0] - buttonPressTime[1] < HOLDDELAY )	)	//same key hit twice in a row and interval between same key presses is less than X milliseconds = key is held
 		{
-			decoder.decode();
-			Serial << "Received " << decoder.value << ", of type: " << decoder.decode_type << "\n";
-			if( decoder.decode_type == NEC)
+			if(keyHeld == 0)
 			{
-				if(decoder.value == 0xFFFFFFFF)
+				timeHoldStarted = buttonPressTime[0];
+				keyHeld = 1;
+			}
+		}
+		else
+		{
+			Previous[1] = Previous[0];
+			Previous[0] = decoder.value;
+			keyHeld = 0;
+		}
+		
+		
+		
+		
+		if(mute == 0)	//not in mute state
+		{
+			if(decoder.value == MUTE)
+			{
+				volumeAtMute = volume;
+				mute = 1;
+				volume = -78;
+				sendVolume();
+			}
+			else if(decoder.value == UP)
+			{
+				// upOrDownPressTime = millis();
+				if(volume < 0)
 				{
-					keyHeld = 1;
-					timeHoldStarted = millis();
-					decoder.value = Previous;
-				}
-				else
-				{
-					keyHeld = 0;
-					Previous = decoder.value;
-				}
-				
-				if(decoder.value == MUTE)
-				{
-					volumeAtMute = volume;
-					mute = 1;
-					volume = -78;
-				}
-				else if(decoder.value == UP)
-				{
-					upOrDownPressTime = millis();
-					if(volume < 0)
+					if(volume + increment() <= 0)
 					{
 						volume += increment();
-					}	
-				}
-				else if(decoder.value == DOWN)
+					}
+					else
+					{
+						volume = 0;
+					}
+					sendVolume();
+				}	
+			}
+			else if(decoder.value == DOWN)
+			{
+				// upOrDownPressTime = millis();
+				if(volume > -79)
 				{
-					upOrDownPressTime = millis();
-					if(volume > -78)
+					if(volume - increment() >= -79)
 					{
 						volume -= increment();
 					}
+					else
+					{
+						volume = -79;
+					}
+					sendVolume();
 				}
-				else if(decoder.value == POWER)
-				{
-					volume = DEFAULTVOLUME;
-				}
+			}
+			else if(decoder.value == POWER)
+			{
+				volume = DEFAULTVOLUME;
 				sendVolume();
 			}
 		}
-	}
-	
-	while(mute == 1)
-	{
-		if(IR.GetResults(&decoder) == 1)
+		else
 		{
 			if(decoder.value == MUTE);
 			{
 				volume = volumeAtMute;
-				mute = 0;
 				sendVolume();
 			}
-			if(decoder.value == UP || decoder.value == DOWN || decoder.value == POWER)			{
+			if(decoder.value == UP || decoder.value == DOWN || decoder.value == POWER)
+			{
 				volume = DEFAULTVOLUME;
-				mute = 0;
 				sendVolume();
 			}
+			mute = 0;
 		}
+		IR.resume();
+		Serial << "Volume: " << volume << " increment: " << increment() << "\n";
 	}
+	
+	
 }
